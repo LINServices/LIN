b7f55da553d4c3f0b2bb705be0a395c6d78047ee
Alex <giraldojhong4@gmail.com>
2024-04-05 14:36:17 -0500

    Member Popup

---
+++ LIN.Stockify/Components/Popup/MemberPopup.razor.cs
@@ -0,0 +1,65 @@
+﻿namespace LIN.Components.Popup;
+
+
+public partial class MemberPopup
+{
+
+    /// <summary>
+    /// Modelo de contacto.
+    /// </summary>
+    private IntegrantDataModel? _data;
+
+
+
+
+
+    /// <summary>
+    /// Obtener / Establecer el modelo.
+    /// </summary>
+    [Parameter]
+    public IntegrantDataModel? Model
+    {
+        get => _data;
+        set
+        {
+            _data = value;
+            InvokeAsync(StateHasChanged);
+        }
+    }
+
+
+
+    /// <summary>
+    /// Abrir el popup.
+    /// </summary>
+    public async void Show()
+    {
+
+        try
+        {
+            // Abrir el popup.
+            await Js.InvokeVoidAsync("ShowModal", "small-modal-member", "closeee-member", "close-btn-send-member");
+        }
+        catch
+        {
+        }
+
+    }
+
+
+
+    /// <summary>
+    /// Abrir el popup.
+    /// </summary>
+    public async void Show(IntegrantDataModel contact)
+    {
+
+        Model = contact;
+        Show();
+
+    }
+
+
+
+
+}
