5e355508587e3c24dabbe55b611bfd1b13a9b2a4
Alex Giraldo <giraldojhong4@gmail.com>
2025-06-02 12:54:20 -0500

    mejoras de ui

--- LIN.Stockify/Components/Pages/Sections/Holds.razor.cs
+++ LIN.Stockify/Components/Pages/Sections/Holds.razor.cs
@@ -2,67 +2,45 @@
 using System.Threading.Tasks;
 
 namespace LIN.Components.Pages.Sections;
-
 public partial class Holds
 {
 
     /// <summary>
-    /// Id.
+    /// Id del inventario de contexto.
     /// </summary>
     [Parameter]
     public string Id { get; set; } = string.Empty;
-
-
 
     /// <summary>
     /// Esta cargando.
     /// </summary>
     private bool IsLoading = false;
 
-
-
-
     /// <summary>
     /// Contexto del inventario.
     /// </summary>
     InventoryContext? Contexto { get; set; }
 
-
+    /// <summary>
+    /// Popup de eliminar.
+    /// </summary>
+    private DeletePopup DeletePopup = null!;
 
     /// <summary>
     /// Respuesta.
     /// </summary>
     private ReadAllResponse<HoldModel>? Response { get; set; } = null;
 
-
-
     /// <summary>
     /// Evento al establecer los parámetros.
     /// </summary>
     protected override void OnParametersSet()
     {
-
         // Obtener el contexto.
         Contexto = InventoryManager.Get(int.Parse(Id));
-
-        // Evaluar el contexto.
-        //if (Contexto != null)
-        //    Response = Contexto.Payments;
-        //else
-        //    Contexto = new()
-        //    {
-        //        Inventory = new()
-        //        {
-        //            Id = int.Parse(Id),
-        //        }
-        //    };
-
         GetData();
-
-        // Base.
         base.OnParametersSet();
     }
-
 
 
     /// <summary>
@@ -70,7 +48,6 @@ public partial class Holds
     /// </summary>
     private async void GetData(bool force = false)
     {
-
         // Validación.
         if (!force && Response != null || IsLoading)
             return;
@@ -80,18 +57,14 @@ public partial class Holds
         StateHasChanged();
 
         // Obtiene los dispositivos
-        var result = await Access.Inventory.Controllers.Holds.ReadAll(Contexto?.Inventory.Id ?? 0 ,Session.Instance.Token);
+        var result = await Access.Inventory.Controllers.Holds.ReadAll(Contexto?.Inventory?.Id ?? 0, Session.Instance.Token);
 
         // Nuevos estados.
         IsLoading = false;
         Response = result;
 
-        //if (Contexto != null)
-        //    Contexto.Payments = Response;
-
         StateHasChanged();
     }
-
 
 
     /// <summary>
@@ -99,12 +72,8 @@ public partial class Holds
     /// </summary>
     public void Render()
     {
-        InvokeAsync(() =>
-        {
-            StateHasChanged();
-        });
+        InvokeAsync(StateHasChanged);
     }
-
 
 
     /// <summary>
@@ -123,38 +92,6 @@ public partial class Holds
     }
 
 
-
-
-    /// <summary>
-    /// Abrir la entradas.
-    /// </summary>
-    void GoEntradas()
-    {
-        nav.NavigateTo($"/inflows/{Contexto?.Inventory.Id}");
-    }
-
-
-
-    /// <summary>
-    /// Abrir integrantes.
-    /// </summary>
-    void GoMembers()
-    {
-        nav.NavigateTo($"/members/{Contexto?.Inventory.Id}");
-    }
-
-
-
-    /// <summary>
-    /// Abrir las salidas.
-    /// </summary>
-    void GoSalidas()
-    {
-        nav.NavigateTo($"/outflows/{Contexto?.Inventory.Id}");
-    }
-
-
-
     /// <summary>
     /// Abrir crear.
     /// </summary>
@@ -163,26 +100,34 @@ public partial class Holds
         nav.NavigateTo($"/new/product/{Contexto?.Inventory.Id}");
     }
 
+
     /// <summary>
-    /// Abrir crear.
+    /// Realizar la devolución de una reserva.
     /// </summary>
-    void GoOpenStore()
-    {
-        nav.NavigateTo($"/openStore/{Contexto?.Inventory.Id}");
-    }
-
-
+    /// <param name="id">Id del grupo de reserva.</param>
     private async Task HoldBack(int id)
     {
-     var response = await   Access.Inventory.Controllers.Holds.Return(id, Session.Instance.Token);
+        var response = await Access.Inventory.Controllers.Holds.Return(id, Session.Instance.Token);
 
         if (response.Response == Responses.Success)
         {
-            Response.Models.RemoveAll(t => t.GroupId == id);
+            Response?.Models.RemoveAll(t => t.GroupId == id);
             StateHasChanged();
         }
-
     }
 
+
+    /// <summary>
+    /// Mostrar el popup de eliminar.
+    /// </summary>
+    public void ShowDelete(int id)
+    {
+        DeletePopup.OnAccept = async () =>
+        {
+            await HoldBack(id);
+        };
+
+        DeletePopup.Show();
+    }
 
 }
